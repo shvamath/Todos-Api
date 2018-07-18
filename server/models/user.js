b09f8527cc591aa74b6b3a0f1a6bdda01db4c8f4
shvamath <shvarczaidmatvei1@gmail.com>
2018-07-18 16:48:40 -0400

    Add delete /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,15 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -80,7 +89,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
           resolve(user);
-        } 
+        }
         reject();
       });
     });
